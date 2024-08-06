89135257fe72c3928aef55639edf272b7a040c17
mmTristan <tristan@mrmxf.com>
2024-08-06 10:27:40 +0100

    more bug hunting

--- encode/examples_test.go
+++ encode/examples_test.go
@@ -23,12 +23,11 @@ func TestStreamEncode(t *testing.T) {
 
 	in := make(chan []byte, 10)
 
-	go func() {
-		for i := 0; i < 10; i++ {
-			in <- []byte(`{"test":true}`)
-		}
-		close(in)
-	}()
+	// fil the channel then close it
+	for i := 0; i < 10; i++ {
+		in <- []byte(`{"test":true}`)
+	}
+	close(in)
 
 	demoConfig := manifest.Configuration{Version: "pre alpha",
 		Default:          manifest.StreamProperties{StreamType: "some data to track", FrameRate: "24/1", NameSpace: "https://metarex.media/reg/MRX.123.456.789.gps"},
@@ -47,7 +46,7 @@ func TestStreamEncode(t *testing.T) {
 		if dat.MRXID != "060e2b34.01020101.0f020101.05000000" {
 			d = dat
 		}
-		fmt.Println(dat.MRXID, dat.FrameRate, len(dat.Data))
+		fmt.Println(dat.MRXID, dat.FrameRate, string(dat.Data[0]))
 	}
 	fmt.Println(len(dec), err, decErr, nonMatchErr)
 	if d != nil {
